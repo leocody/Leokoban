433f9c05792ecb74d0cc4abb1d1ac6696cb09ad5
Leo <leo@Mac-mini.local>
2020-05-06 14:56:51 +0900

    Step Count and Reset

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 import pyxel
 from player import Player
 from box import Box
-from constant import PYSIZE, HALFPYSIZE
-
+from constant import PYSIZE, HALFPYSIZE, SCREEN_HEIGHT, SCREEN_WIDTH
 COURSE_LAYOUT = [
     [2, 2, 2, 2, 2],
     [2, 0, 0, 0, 2],
@@ -11,8 +10,7 @@ COURSE_LAYOUT = [
     [2, 2, 2, 2, 2]
 ]
 
-SCREEN_WIDTH = 5 * PYSIZE
-SCREEN_HEIGHT = 5 * PYSIZE
+
 EMPTY = 0
 PLAYER = 1
 WALL = 2
@@ -21,8 +19,7 @@ GOAL = 4
 class App:
     def __init__(self, course):
         self.course = course
-        self.player = Player(1, 2)
-        self.box = Box(2, 2)
+        self.reset()
         pyxel.init(SCREEN_WIDTH, SCREEN_HEIGHT, caption="Leokoban")
         pyxel.load("assets/images.pyxres")
         pyxel.run(self.draw, self.update)
@@ -33,7 +30,9 @@ class App:
 
         if pyxel.btnp(pyxel.KEY_UP) or pyxel.btnp(pyxel.GAMEPAD_1_UP):
             if self.box.x == self.player.x and self.box.y == self.player.y - 1:
-                self.player.push_and_move_up(self.box)
+                box_next_pos = self.course[self.box.y - 1][self.box.x]
+                if box_next_pos != WALL:
+                    self.player.push_and_move_up(self.box)
             else:
                 nextposition = self.course[self.player.y - 1][self.player.x]
                 if nextposition != WALL:
@@ -41,7 +40,9 @@ class App:
         
         if pyxel.btnp(pyxel.KEY_DOWN) or pyxel.btnp(pyxel.GAMEPAD_1_DOWN):
             if self.box.x == self.player.x and self.box.y == self.player.y + 1:
-                self.player.push_and_move_down(self.box)
+                box_next_pos = self.course[self.box.y + 1][self.box.x]
+                if box_next_pos != WALL:
+                    self.player.push_and_move_down(self.box)
             else:
                 nextposition = self.course[self.player.y + 1][self.player.x]
                 if nextposition != WALL:
@@ -49,7 +50,9 @@ class App:
         
         if pyxel.btnp(pyxel.KEY_LEFT) or pyxel.btnp(pyxel.GAMEPAD_1_LEFT):
             if self.box.x == self.player.x - 1 and self.box.y == self.player.y:
-                self.player.push_and_move_left(self.box)
+                box_next_pos = self.course[self.box.y][self.box.x - 1]
+                if box_next_pos != WALL:
+                    self.player.push_and_move_left(self.box)
             else:
                 nextposition = self.course[self.player.y][self.player.x - 1]
                 if nextposition != WALL:
@@ -57,16 +60,36 @@ class App:
         
         if pyxel.btnp(pyxel.KEY_RIGHT) or pyxel.btnp(pyxel.GAMEPAD_1_RIGHT):
             if self.box.x == self.player.x + 1 and self.box.y == self.player.y:
-                self.player.push_and_move_right(self.box)
+                box_next_pos = self.course[self.box.y][self.box.x + 1]
+                if box_next_pos != WALL:
+                    self.player.push_and_move_right(self.box)
             else:
                 nextposition = self.course[self.player.y][self.player.x + 1]
                 if nextposition != WALL:
-                    self.player.move_right()   
-        
+                    self.player.move_right() 
 
-    #DRAW LOGIC   
+        if pyxel.btnp(pyxel.KEY_R) or pyxel.btnp(pyxel.GAMEPAD_1_B):
+            self.reset()
+            
+        self.check_complete()
+
+    def check_complete(self):
+        box_pos = self.course[self.box.y][self.box.x]
+        if box_pos == GOAL:
+            self.goal = True
+
+    #RESET LOGIC
+    def reset(self):
+        self.player = Player(1, 2)
+        self.box = Box(2, 2)
+        self.goal = False
+
+
+
+    #DRAW LOGIC  
     def draw(self):
         pyxel.cls(pyxel.COLOR_BLACK)
+
         for y, line in enumerate(self.course):
             for x, square in enumerate(line):
                 if square == EMPTY:
@@ -83,8 +106,20 @@ class App:
                     pyxel.blt(x * PYSIZE, y * PYSIZE, 0, 0, 16, 16, 16)
                 else:
                     print("Unknown")
+
         self.player.draw()
         self.box.draw()
+
+        #CHECK IF IT IS GOAL
+
+        if self.goal:
+            pyxel.text(
+                SCREEN_WIDTH / 2 - 35, 
+                SCREEN_HEIGHT / 2,
+                "GOAL! YOU MADE IT!",
+                11)
+
+    
 
 
 App(COURSE_LAYOUT)
--- player.py
+++ player.py
@@ -1,24 +1,29 @@
 import pyxel
-from constant import PYSIZE, HALFPYSIZE
+from constant import PYSIZE, HALFPYSIZE, SCREEN_HEIGHT, SCREEN_WIDTH
 
 class Player:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+        self.step = 0
 
     #Move Logic
 
     def move_up(self):
         self.y -= 1
+        self.step += 1
 
     def move_down(self):
         self.y += 1
+        self.step += 1
 
     def move_left(self):
         self.x -= 1
+        self.step += 1
 
     def move_right(self):
         self.x += 1
+        self.step += 1
 
     #Push Logic
 
@@ -29,6 +34,7 @@ class Player:
     def push_and_move_down(self, box):
         box.move_down()
         self.move_down()
+
 
     def push_and_move_left(self, box):
         box.move_left()
@@ -42,4 +48,6 @@ class Player:
     #DRAW LOGIC
     def draw(self):
         pyxel.blt(self.x * PYSIZE, self.y * PYSIZE, 0, 16, 0, 16, 16)
+        pyxel.text(1, SCREEN_HEIGHT - 9, "Walk Step : " + str(self.step), pyxel.COLOR_WHITE)
+
 
